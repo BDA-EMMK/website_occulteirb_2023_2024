51379c1a36ab98e2d8ac3f9401c0271b444cefc6
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2024-02-10 12:49:08 +0100

    [Svelte config] Create layout for app and enable static generation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+
+<script>
+import Header from "./header.svelte";
+// import Footer from './footer.svelte';
+
+export const prerender = true;
+</script>
+
+<Header />
+
+<main>
+  <slot />
+</main>
+
+<!-- <Footer /> -->
+
+<style lang="scss">
+</style>
+
