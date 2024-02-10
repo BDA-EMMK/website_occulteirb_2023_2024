b4f364c3ce3026d07cc0c513f87bdbd1a283d809
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2024-02-10 11:59:17 +0100

    [Migration] Start migration of header component

---
+++ src/routes/header.svelte
@@ -0,0 +1,61 @@
+
+<script>
+  const name = "World";
+
+</script>
+
+<header class="header">
+  <!-- Left side logo -->
+  <a href="./index.html" class="logo-link">
+    <img class="logo" src="pics/logos/eye_logo.png" alt="Logo">
+  </a>
+  
+  <!-- Navbar to the right -->
+  <nav>
+    <ul class="navbar">
+      <li>
+        <a href="./index.html" class="nav-link special-link active">
+          <p>Accueil</p>
+        </a>
+      </li>
+
+      <li>
+        <a href="./equipe.html" class="nav-link">
+          <p>Equipe</p>
+        </a>
+      </li>
+
+      <li>
+        <a href="./allo.html" class="nav-link">
+          <p>Allo</p>
+        </a>
+      </li>
+
+      <li>
+        <a href="./partenaires.html" class="nav-link">
+          <p>Partenaires</p>
+        </a>
+      </li>
+
+      <li>
+        <a href="./video.html" class="nav-link">
+          <p>Video</p>
+        </a>    
+      </li>
+
+      <li>
+        <a href="./jeu.html" class="nav-link">
+          <p>Jeux</p>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+
+<style lang="scss">
+nav {
+  background: red;
+}
+
+</style>
