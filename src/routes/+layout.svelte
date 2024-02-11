85312491dbad38a8dab70e7140ef35a1defc9443
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2024-02-11 10:49:04 +0100

    [Fix] Move prerender opt to +layout.ts -> static generation works

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,6 @@
 import Header from "./header.svelte";
 import Footer from './footer.svelte';
 
-// Used for static generation, ive no idea what is does
-export const prerender = true;
 </script>
 
 <Header />
---
+++ src/routes/+layout.ts
@@ -0,0 +1,3 @@
+
+// Used for static generation, ive no idea what is does
+export const prerender = true;
