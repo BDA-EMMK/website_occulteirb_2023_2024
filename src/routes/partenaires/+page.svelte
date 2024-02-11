8a4d4e406bb685162cc4ed72142b21707a455fcc
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2024-02-11 09:41:48 +0100

    [Migration][Partners] Remove css for partnerCard and use partnerCard component

--- src/routes/partenaires/+page.svelte
+++ src/routes/partenaires/+page.svelte
@@ -1,8 +1,25 @@
 
 <script lang="ts" context="module">
 export type Partner = {
-  
+  name: string;
+  imgURL: string;
+  desc: string;
 }
+
+import PartnerCard from "./partnerCard.svelte";
+
+const partners: Partner[] = [
+  {
+    name: "openAI",
+    imgURL: "/img/troll/openai.png",
+    desc: "Des randoms"
+  },
+  {
+    name: "The Rock !",
+    imgURL: "/img/troll/the_rock.jpg",
+    desc: "Un random"
+  }
+]
 
 </script>
 
@@ -14,15 +31,9 @@ export type Partner = {
 <section class="pole-section">
   <div class="partners">
 
-    <!-- Member 1 -->
-    <div class="team-member">
-      <img src="/img/troll/openai.png" alt="Membre 1" id="image1">
-    </div>
-
-    <!-- Member 2 -->
-    <div class="team-member">
-      <img src="/img/troll/the_rock.jpg" alt="Membre 2">
-    </div>
+    {#each partners as partner}
+      <PartnerCard partner={partner} />
+    {/each}
 
  </div>
 </section>
@@ -34,68 +45,6 @@ export type Partner = {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
-}
-
-.team-member {
-  width: 250px;
-  height: 400px;
-  margin: auto;
-  text-align: center;
-  object-fit: cover;
-  position: relative;
-  overflow: hidden;
-  transition: transform 0.3s ease-in-out;
-  transform: translateX(-150px);
-}
-
-.team-member::before {
-	z-index: -1;
-	position: absolute;
-	content: '';
-	width: 150px;
-	height: 500px;
-	background: rgba(255, 73, 1, 1);
-	transform: rotate(45deg);
-}
-
-.team-member:hover::before {
-	animation: animate 2s linear infinite;
-}
-
-@keyframes animate{
-	from {
-		transform: rotate(0deg);
-	}
-	to {
-
-		transform: rotate(362deg);
-	}
-}
-
-.team-member::after {
-	z-index: -1;
-	position: absolute;
-	content: '';
-	inset: 4px;
-	border-radius: 8px;
-}
-
-.team-member img {
-  width: 96%;
-  height: 96%;
-  object-fit: cover;
-  border-radius: 6px;
-  transform: translateY(9px);
-}
-
-.member-program {
-  margin: 0;
-  font-size: 14px;
-  color: #666;
-}
-
-.team-member:hover {
-  transform: scale(1.05);
 }
 </style>
 
