b4215e0709b3005e7e54621b49d70f014d42c6f4
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2024-02-11 19:29:24 +0100

    [UI] Play with fonts, added Lorem Ipsum to /

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,45 @@
 
-<script>
+<script lang="ts">
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<div class="texts">
+  <p class="text">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+  <p class="text">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic delectus accusamus vero quos ea non. Velit, ex ipsam beatae enim nulla libero incidunt autem veritatis, dicta voluptate dolorum fuga reprehenderit.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nulla, non ipsam minima odio tempora iure quidem. Nostrum pariatur perferendis illum! Distinctio sed delectus perferendis nulla minus culpa mollitia velit!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dolor tempore cum beatae iste voluptatum modi velit quam natus eos quasi voluptate saepe consequuntur, iure necessitatibus! Qui nihil dolores iste.
+  </p>
+</div>
+
+<div class="texts">
+  <p class="text">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam itaque porro tempore! Tenetur eligendi temporibus nulla iure similique dolorum velit, atque rem sed, officiis corporis reiciendis, aperiam rerum quas quis.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, obcaecati. Odio sint laudantium iusto. Ratione fuga cumque maxime, odit veritatis voluptates. Quae excepturi consequatur animi ipsam! Cumque suscipit reprehenderit autem.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat distinctio saepe, culpa commodi consectetur odio voluptate vero, magni expedita recusandae sequi necessitatibus laborum itaque eligendi. Nisi provident laudantium quibusdam officia.
+  </p>
+
+  <div class="img"></div>
+</div>
+
+<style lang="scss">
+
+div.texts {
+  display: flex;
+  
+  justify-content: space-between;
+
+
+  padding: 2em 5em;
+}
+
+p.text {
+  margin: 0;
+  max-width: 65ch;
+}
+
+</style>
 
 
