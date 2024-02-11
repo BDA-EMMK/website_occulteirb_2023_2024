ca9b8228120ba53df2e986711fa791b779d236e2
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2024-02-11 09:48:01 +0100

    [Migration][Footer] Create the footer component

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,5 +1,20 @@
 
 <footer>
   <h3>Ceci est un footer de qualité !</h3>
+  <img src="/img/portrait_2d.png" alt="Description de l'image" class="bottom-image">
 </footer>
+
+<style lang="scss">
+footer {
+  width: 100%;
+  height: 1svh;
+
+  position: relative;
+
+  img {
+    position: relative;
+    width: 100%;
+  }
+}
+</style>
 
