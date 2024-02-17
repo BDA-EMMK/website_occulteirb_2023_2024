5f36b6cd5dc0eda612508e6a33068ffddfbbb4d1
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2024-02-17 11:04:45 +0100

    [UI] Create basic loading screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 
-<script>
+<script lang="ts">
 import Header from "./header.svelte";
 import Footer from './footer.svelte';
 
+import { onMount } from "svelte";
+
+let loaded: boolean = false
+let endloaded: boolean = false
+
+onMount(() => {
+  loaded = true;
+
+  setTimeout(() => { endloaded = true }, 1000)
+});
 </script>
+
+<div class="loading" class:loaded class:endloaded></div>
 
 <Header />
 
@@ -15,6 +27,50 @@ import Footer from './footer.svelte';
 
 <style lang="scss">
 @import '$lib/theme.scss';
+
+@keyframes loading-screen {
+	from {
+    opacity: 0;
+	}
+	to {
+    opacity: 100%;
+	}
+}
+
+.loading {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+
+  z-index: 999;
+
+  background-color: yellow;
+
+  transition: opacity 1s;
+  pointer-events: none;  /* Do not prevent from click nav ... */
+
+  &::after {
+    content: "";
+
+    position: absolute;
+    left: 0;
+    top: 0;
+
+    width: 100%;
+    height: 100%;
+
+    background-color: red;
+		animation: 1s ease-in-out 0s infinite alternate loading-screen;
+  }
+
+  &.loaded {
+    opacity: 0;
+  }
+
+  &.endloaded {
+    display: none;
+  }
+}
 
 main {
   overflow-y: hidden;
