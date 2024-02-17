95b8c1d82665ae0ba3bdf1a434490ed36b02e006
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2024-02-17 09:46:10 +0100

    [UI] Remove footer img cause it's annoying

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,13 +1,25 @@
 
 <footer>
   <h3>Ceci est un footer de qualité !</h3>
-  <img src="/img/portrait_2d.png" alt="Description de l'image" class="bottom-image">
+  <!-- <img src="/img/portrait_2d.png" alt="Description de l'image" class="bottom-image"> -->
 </footer>
 
 <style lang="scss">
+@import "$lib/theme.scss";
+
 footer {
   width: 100%;
   height: 1svh;
+
+  h3 {
+    margin: 0;
+    padding: 0;
+
+    background-color: inherit;
+  }
+
+  background-color: $background;
+  color: $foreground;
 
   position: relative;
 
