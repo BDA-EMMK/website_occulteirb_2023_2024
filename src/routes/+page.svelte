1aa76fd298bcadf056e28c2ed1a5dc008d8be34b
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2024-02-19 22:06:41 +0100

    [UI] index page: Add container for brasse and better padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,24 +43,31 @@ const socialLinks: SocialLinkData[] = [
   </ul>
 </div>
 
-<div class="texts">
-  <p class="text">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="brasse">
+<h2>Time for the brasse !</h2>
 
-  <p class="text">
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic delectus accusamus vero quos ea non. Velit, ex ipsam beatae enim nulla libero incidunt autem veritatis, dicta voluptate dolorum fuga reprehenderit.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nulla, non ipsam minima odio tempora iure quidem. Nostrum pariatur perferendis illum! Distinctio sed delectus perferendis nulla minus culpa mollitia velit!
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dolor tempore cum beatae iste voluptatum modi velit quam natus eos quasi voluptate saepe consequuntur, iure necessitatibus! Qui nihil dolores iste.
-  </p>
+<div class="brasse-items">
+  <div class="texts">
+    <p class="text">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+    <p class="text">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic delectus accusamus vero quos ea non. Velit, ex ipsam beatae enim nulla libero incidunt autem veritatis, dicta voluptate dolorum fuga reprehenderit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nulla, non ipsam minima odio tempora iure quidem. Nostrum pariatur perferendis illum! Distinctio sed delectus perferendis nulla minus culpa mollitia velit!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dolor tempore cum beatae iste voluptatum modi velit quam natus eos quasi voluptate saepe consequuntur, iure necessitatibus! Qui nihil dolores iste.
+    </p>
+  </div>
+
+  <div class="texts">
+    <p class="text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam itaque porro tempore! Tenetur eligendi temporibus nulla iure similique dolorum velit, atque rem sed, officiis corporis reiciendis, aperiam rerum quas quis.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, obcaecati. Odio sint laudantium iusto. Ratione fuga cumque maxime, odit veritatis voluptates. Quae excepturi consequatur animi ipsam! Cumque suscipit reprehenderit autem.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat distinctio saepe, culpa commodi consectetur odio voluptate vero, magni expedita recusandae sequi necessitatibus laborum itaque eligendi. Nisi provident laudantium quibusdam officia.
+    </p>
+
+    <div class="img"></div>
+  </div>
 </div>
 
-<div class="texts">
-  <p class="text">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam itaque porro tempore! Tenetur eligendi temporibus nulla iure similique dolorum velit, atque rem sed, officiis corporis reiciendis, aperiam rerum quas quis.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, obcaecati. Odio sint laudantium iusto. Ratione fuga cumque maxime, odit veritatis voluptates. Quae excepturi consequatur animi ipsam! Cumque suscipit reprehenderit autem.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat distinctio saepe, culpa commodi consectetur odio voluptate vero, magni expedita recusandae sequi necessitatibus laborum itaque eligendi. Nisi provident laudantium quibusdam officia.
-  </p>
-
-  <div class="img"></div>
 </div>
 
 <style lang="scss">
@@ -86,7 +93,7 @@ const socialLinks: SocialLinkData[] = [
 
 
 .logo {
-	width: 100vw;
+	width: 100%;
 	height: 100svh;
 
 	display: flex;
@@ -121,7 +128,7 @@ const socialLinks: SocialLinkData[] = [
 		top: 0;
 
 		border-radius: 100%;
-		background-color: $red;
+		background-color: $logo-red;
 
 		animation: 60s linear 0s infinite logo-rotation;
 	}
@@ -133,13 +140,12 @@ div.texts {
   
   justify-content: space-between;
 
-
-  padding: 2em 5em;
+  padding: 2em 10vw;
 }
 
 p.text {
   margin: 0;
-  max-width: 65ch;
+  max-width: 60ch;
 }
 
 .social-links {
@@ -147,6 +153,8 @@ p.text {
 
   bottom: 2vw;
   left: 2vw;
+
+  z-index: 1;
 
   ul {
     margin: 0;
