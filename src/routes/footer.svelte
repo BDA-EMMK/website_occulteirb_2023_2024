d82b57eafddc7265331cb01f3ba198984e151410
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2024-02-25 17:27:00 +0100

    [Footer] Add links to footer, new background and spacing

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,33 +1,89 @@
 
+<script lang="ts">
+import type { HeaderNavData } from './headerNav';
+
+const headerNavDatas: HeaderNavData[] = [
+  {
+    URL: '/',
+    text: 'Accueil'
+  },
+  {
+    URL: '/equipe/',
+    text: 'Équipe'
+  },
+  {
+    URL: '/allo/',
+    text: 'Allos'
+  },
+  {
+    URL: '/video/',
+    text: 'Vidéo'
+  },
+  {
+    URL: '/jeu/',
+    text: 'Jeu'
+  },
+  {
+    URL: '/partenaires/',
+    text: 'Partenaires'
+  },
+];
+</script>
+
 <footer>
-  <h3>Ceci est un footer de qualité !</h3>
-  <!-- <img src="/img/portrait_2d.png" alt="Description de l'image" class="bottom-image"> -->
+  <h3>Destin'eirb</h3>
+
+	<ul class="footer-links">
+		{#each headerNavDatas as headerNavData}
+			<li class="footer-link">
+				<a href="{ headerNavData.URL }">
+					{ headerNavData.text }
+				</a>
+			</li>
+		{/each}
+	</ul>
+
+	<p class="text copyright">
+		&copy; Destin'eirb &mdash; All rights reserved.
+	</p>
 </footer>
 
 <style lang="scss">
 @import "$lib/theme.scss";
 
 footer {
-  width: 100%;
-  height: 1svh;
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+
+	padding: 5rem 2rem;
+
+  position: relative;
 
   h3 {
     margin: 0;
     padding: 0;
 
+		font-size: 4rem;
+
     background-color: inherit;
   }
 
-  background-color: $background;
+  background-color: $darker-background;
   color: $foreground;
+}
 
-  position: relative;
+.footer-links {
+	display: flex;
+	flex-direction: column;
 
-  img {
-    z-index: -1;
-    position: relative;
-    width: 100%;
-  }
+	align-items: flex-end;
+
+	font-size: 3rem;
+}
+
+.copyright {
+	text-align: center;
 }
 </style>
 
