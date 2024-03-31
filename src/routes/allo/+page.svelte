283d5f1283455911e2020421d0d57ea63bdc6243
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2024-03-31 18:46:08 +0200

    [fix&feat][allo] ask for auth to show hist, fetch hist from serv

--- src/routes/allo/+page.svelte
+++ src/routes/allo/+page.svelte
@@ -1,10 +1,16 @@
 
 <script lang="ts">
-import type { Allo } from "./allo";
 import AlloCard from "./alloCard.svelte";
 import AlloHero from "./alloHero.svelte";
 
-const allos: Allo[] = [
+import type { Allo } from "$lib/allo";
+import Allos from "$lib/allo";
+
+import Auth from "$lib/auth";
+import { onMount } from "svelte";
+    import LoginButton from "$lib/login-button.svelte";
+
+let allos: Allo[] = [
   {
     UID: "0",
     CAS_UID: "jsp",
@@ -32,31 +38,70 @@ const allos: Allo[] = [
     creationDate: "5 min ago",
     requestText: "Des crèpes pitié monsieur...."
   }
-]
+];
+
+let ticket: string = "";
+onMount(async () => {
+	ticket = Auth.getToken() ;
+
+	if (Auth) {
+		console.log(allos);
+		allos = await Allos.getAllos(ticket);
+		console.log(allos);
+	}
+
+});
+
 </script>
 
 
 <AlloHero />
 
 <section id="allo">
-<ul class="allos-list">
-  {#each allos as allo}
-    <li>
-      <AlloCard allo={allo} />
-    </li>
-  {/each}
-</ul>
+	{#if ticket === ""}
+		<div class="no-auth">
+			<h2>Authentifiez vous pour accéder à l'historique des demandes !</h2>
+			<LoginButton />
+		</div>
+
+	{:else}
+		<ul class="allos-list">
+			{#each allos as allo}
+				<li>
+					<AlloCard allo={allo} />
+				</li>
+			{/each}
+		</ul>
+	{/if}
 </section>
 
 
 <style lang="scss">
 @import '$lib/theme.scss';
 
-@media screen and (max-width: 899px) {
+.no-auth {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+
+	width: 100%;
+	height: 100svh;
+	gap: 5rem;
+
+	h2 {
+		font-size: 3rem;
+	}
+}
 
 #allo {
 	background-color: $alt-background;
+	position: relative;
+
+	min-height: 100svh;
 }
+
+@media screen and (max-width: 899px) {
 
 .allos-list {
 	padding: 5vw 0vw;
