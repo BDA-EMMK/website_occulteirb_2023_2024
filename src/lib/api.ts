73bedfb8ce4ae8a1a3d936c4fd8f9fa9cdbf34b5
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2024-03-31 18:40:57 +0200

    [feat][api] moved some logic of api in its own lib

---
+++ src/lib/api.ts
@@ -0,0 +1,93 @@
+
+export interface HeadQuarter {
+	name: string;
+	address: string;
+	localStudents: string[];
+}
+
+// export interface Allo = {
+//   UID: string;
+//   CAS_UID: string;
+//   state: "Finished" | "In Progress";
+//   title: string;
+//   creationDate: string;
+//   finishDate: string;
+//   requestText: string;
+// };
+
+export interface Allo {
+	id: string;
+	clientStudentId: number;
+	clientStudent: string;
+	assignedListeux: object[];
+	creationDate: string;
+	updatedOn: string;
+	quantity: number;
+	state: string;
+	taskId: number;
+	task: {
+		taskId: number;
+		taskName: string;
+		price: number;
+		picture: {};
+		description: string
+	};
+	requestText: string;
+	finishData: string
+}
+
+export interface StudentData {
+  CASId: string;
+  CASUId: string;
+  firstName: string;
+  completeName: string;
+  nickname: string;
+  mail: string;
+  school: string;
+  pathway: string;
+  address: string;
+  profilePicture: {};
+  relatedHqId: 0;
+  relatedHq: HeadQuarter;
+  phone: string;
+  allos: Allo[];
+  score: 0;
+}
+
+const API_PREFIX = 'http://localhost:3000';
+
+export function APIUrl(route: string): string {
+	return API_PREFIX + route;
+}
+
+export async function getStudentData(ticket: string): Promise<StudentData> {
+	const requestURL = APIUrl('/student');
+	const init: RequestInit = {
+		headers: [
+			['Authorization', `Bearer ${ticket}`],
+		],
+	};
+
+	const res = await (fetch(requestURL, init).catch(e => {
+			console.error(e);
+			throw new Error("GET /student request failed; request: " + requestURL);
+		})
+	);
+
+	switch(res.status) {
+		case 401:
+			throw new Error("Need auth: " + requestURL);
+
+		case 200:
+			return await res.json() as StudentData;
+
+		default:
+			throw new Error("Unhandled submitAllo statusCode: " + res.status)
+	}
+}
+
+export default {
+	getStudentData,
+	APIUrl
+}
+
